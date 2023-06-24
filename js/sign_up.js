123feecff30ab455fd01f11cda2fc5c318919d2c
unigwesunday7 <unigwesunday7@gmail.com>
2023-06-24 12:43:13 +0100

    Sign up form validation

--- js/sign_up.js
+++ js/sign_up.js
@@ -1,17 +1,101 @@
+const form = document.getElementById('form');
+const fullnames = document.getElementById('fullnames');
+const country = document.getElementById('country');
+const phone = document.getElementById('phone');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-let formInput = doc.getElementById('formInput');
-let form = document.getElementById('signUp');
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-form.addEventListener('submit', function(event) {
+    validateInputs();
+});
 
-    
-  if (!formInput.value) {
-      message1.innerText = "Please enter values in the fields";
-      event.preventDefault();
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+  const usernameValue = fullnames.value.trim();
+  const countryValue = country.value.trim();
+  const phoneValue = phone.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+
+  
+  if(usernameValue === '') {
+      setError(fullnames, 'Fullnames are required');
+  } else {
+      setSuccess(fullnames);
   }
 
+  if(countryValue === 'select country') {
+    setError(country, 'Please select country');
+  } else {
+    setSuccess(country);
+  }
 
-});
+  if(phoneValue === '') {
+  setError(phone, 'Phone number is required');
+  } else if (phoneValue.length < 10) {
+    setError(phone, 'Phone number must be at least 10 digits.');
+  } else {
+  setSuccess(phone);
+  }
+
+  if(emailValue === '') {
+      setError(email, 'Email is required');
+  } else if (!isValidEmail(emailValue)) {
+      setError(email, 'Provide a valid email address');
+  } else {
+      setSuccess(email);
+  }
+
+  if(passwordValue === '') {
+      setError(password, 'Password is required');
+  } else if (passwordValue.length < 8 ) {
+      setError(password, 'Password must be at least 8 character.');
+  } else {
+      setSuccess(password);
+  }
+
+  if(password2Value === '') {
+      setError(password2, 'Please confirm your password');
+  } else if (password2Value !== passwordValue) {
+      setError(password2, "Passwords doesn't match");
+  } else {
+      setSuccess(password2);
+  }
+
+};
+
+
+
+
 
 
 
@@ -23,24 +107,24 @@ function country_code() {
     let val = document.getElementById("country").value;
 
     if (val === "select_country") {
-      document.getElementById("output").value = "";
+      document.getElementById("phone").value = "";
     }
     else if (val === "ng") {
-      document.getElementById("output").value = "+234";
+      document.getElementById("phone").value = "+234";
     }
     else if (val === "gh") {
-      document.getElementById ("output").value = "+233";
+      document.getElementById ("phone").value = "+233";
     }
     else if (val === "us") {
-      document.getElementById ("output").value = "+1";
+      document.getElementById ("phone").value = "+1";
     }
     else if (val === "sa") {
-      document.getElementById ("output").value = "+27";
+      document.getElementById ("phone").value = "+27";
     }
     else if (val === "uk") {
-      document.getElementById ("output").value = "+380";
+      document.getElementById ("phone").value = "+380";
     }
-  };
+  }
 
 
   function togglePW1(){
@@ -54,7 +138,7 @@ function country_code() {
         password.setAttribute('type', 'password');
         document.getElementById('eye').style.color='#000';
     }
-  };
+  }
 
     function togglePW2() {
       let password = document.getElementById('password2');
