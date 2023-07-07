77ed1a77bc9a340f073e8ed083796a5d84140347
unigwesunday7 <unigwesunday7@gmail.com>
2023-07-07 16:14:32 +0100

    navbar modified

---
+++ js/library.js
@@ -0,0 +1,9 @@
+// ========= Nav Links Responsive Toggle ==========
+function userToggle() {
+    let userProfile = document.getElementById("myTopnav");
+    if (userProfile.className === "navLinks") {
+      userProfile.className += " responsive";
+    } else {
+      userProfile.className = "navLinks";
+    }
+  }
